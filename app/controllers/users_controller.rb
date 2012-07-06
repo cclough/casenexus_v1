6e48bbec378892c61a643d8f399677ef2026f372
Christian Clough <christian.clough@gmail.com>
2012-07-07 00:59:41 +0100

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-   def show
+
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
   	@user = User.find(params[:id])
   end
 
@@ -16,5 +25,43 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,16 +26,16 @@ describe "AuthenticationPages" do
 		end
 
 		describe "with valid information" do
+
 			let(:fguser) { FactoryGirl.create(:user) }
-			before do
-				fill_in "Email", with: fguser.email
-				fill_in "Password", with: fguser.password
-				click_button "Sign in"
-			end
+			
+			before { sign_in fguser }
 
 			it { should have_selector('title', text: fguser.name) }
 			it { should have_link('Profile', href: user_path(fguser)) }
 			it { should have_link('Sign out', href: signout_path) }
+			it { should have_link('Settings', href: edit_user_path(fguser)) }
+			it { should have_link('Users', href: users_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
 
 			describe "followed by signout" do
@@ -44,5 +44,93 @@ describe "AuthenticationPages" do
 			end
 		end
 	end
+
+	describe "authorisation" do
+
+		describe "for non-signed-in users" do
+			let(:fguser) { FactoryGirl.create(:user)}
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(fguser)
+					fill_in "Email", with: fguser.email
+					fill_in "Password", with: fguser.password
+					click_button "Sign in"
+				end
+	
+				describe "after signing in" do
+					it "should render the desired protected page" do
+						page.should have_selector('title', text: 'Edit user')
+					end
+				
+					describe "when signing in again" do
+						before do
+							click_link "Sign out"
+							click_link "Sign in"
+							fill_in "Email", with: fguser.email
+							fill_in "Password", with: fguser.password
+							click_button "Sign in"
+						end
+
+						it "should render the default (profile) page" do
+							page.should have_selector('title', test: fguser.name)
+						end
+					end
+
+				end
+
+
+			end
+
+			describe "in the Users controller" do
+
+				describe "visiting the edit page" do
+					before { visit edit_user_path(fguser) }
+					it { should have_selector('title',text: 'Sign in') }
+					it { should have_selector('div.alert.alert-notice') }
+				end
+
+				describe "submitting to the update action" do
+					before { put user_path(fguser) }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+			end
+		end
+
+		describe "as wrong user" do
+			let(:fguser) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user,email: "wrong@example.com") }
+			before { sign_in fguser }
+
+			describe "visiting Users#edit page" do
+				before { visit edit_user_path(wrong_user) }
+				it { should_not have_selector('title',text: 'Edit user') }
+			end
+
+			describe "submitting a PUT request to the Users#update action" do
+				before { put user_path(wrong_user) }
+				specify { response.should redirect_to(root_path) }
+			end
+	 	end
+
+	 	describe "as non-admin user" do
+	 		let(:user) { FactoryGirl.create(:user) }
+	 		let(:non_admin) { FactoryGirl.create(:user) }
+
+	 		before { sign_in non_admin }
+
+	 		describe "submitting a DELETE request to the Users#destroy action" do
+	 			before { delete user_path(user) }
+	 			specify { response.should redirect_to(root_path) }
+	 		end
+	 	end
+
+	end
+
 
 end
