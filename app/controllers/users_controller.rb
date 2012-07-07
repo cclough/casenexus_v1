3513e098b31c0652687e50ae2fc65005e70457ab
Christian Clough <christian.clough@gmail.com>
2012-07-08 00:33:48 +0100

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
       redirect_to @user 
